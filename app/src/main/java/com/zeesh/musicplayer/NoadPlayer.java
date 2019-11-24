f54e094042867029dd7d13d566aac0ce9b88b716
Muhammad Zeeshan Ashraf <zeeshanashraf633b@gmail.com>
2019-11-24 18:39:01 +0500

    Sky Beat 0.1

---
+++ app/src/main/java/com/zeesh/musicplayer/NoadPlayer.java
@@ -0,0 +1,10 @@
+package com.zeesh.musicplayer;
+
+import android.app.Application;
+
+public class NoadPlayer extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+    }
+
+}
---
+++ app/src/main/java/com/zeesh/musicplayer/ui/callbacks/AsyncTaskCompletionCallback.java
@@ -0,0 +1,5 @@
+package com.zeesh.musicplayer.ui.callbacks;
+
+public interface AsyncTaskCompletionCallback {
+    void updateViews();
+}
